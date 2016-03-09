8d2e1a1a67c27a229f7db55d5c3f8dab3189554c
Sayantan Guha <sayantanguha@hotmail.com>
2016-03-09 04:04:40 -0700

    Implementation of Binary Tree using Single Array is implemeted

---
+++ DS.Through.Java/src/Trees/SingleArrayBinaryTree.java
@@ -0,0 +1,55 @@
+package Trees;
+
+class BTNode
+{
+	char data;
+	BTNode left, right;
+	
+	BTNode(char a)
+	{
+		this.data =  a;
+		left =null;
+		right =  null;
+	}
+}
+
+class BTree
+{
+	
+
+	
+	public BTNode buidtree(int n, char[] a)
+	{
+		BTNode node=null;
+		if(a[n]!='\0')
+		{
+			node = new BTNode(a[n]);
+			node.left =  buidtree(2*n+1, a);
+			node.data = a[n];
+			node.right = buidtree(2*n+2, a);
+			
+		}
+		return node;
+	}
+}
+
+public class SingleArrayBinaryTree {
+
+	public static void main(String[] args) {
+		char a[]={'A','B','C','D','E','F','G','\0','\0','H','\0',
+				'\0','\0','\0','\0','\0','\0','\0','\0','\0','\0'};
+		BTree tree = new BTree();
+		BTNode temp = tree.buidtree(0, a);
+		printInorder(temp);
+
+	}
+	static void printInorder(BTNode node)
+	{
+		if(node == null)
+			return;
+		printInorder(node.left);
+		System.out.print(node.data + " ");
+		printInorder(node.right);
+	}
+	
+}
