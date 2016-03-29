750f817bfa15d0e92a9079bffccb9593f3ec4c4d
Sayantan Guha <sayantanguha@hotmail.com>
2016-03-28 23:39:18 -0700

    Level Order Tarversal using Queue

---
+++ DS.Through.Java/src/Trees/BinaryTree/LevelOrderTraversalQueue.java
@@ -0,0 +1,70 @@
+package Trees.BinaryTree;
+
+import java.util.*;
+
+public class LevelOrderTraversalQueue {
+	class Node
+	{
+		int data;
+		Node left, right;
+		Node(int value)
+		{
+			this.data = value;
+			this.left=null;
+			this.right=null;
+		}
+	}
+	class BinaryTree
+	{
+		Node root;
+		public BinaryTree(int data) {
+			root = new Node(data);
+		}
+		public BinaryTree()
+		{
+			root=null;
+		}
+		
+		void printBFS(Node root)
+		{
+			Queue<Node> queue =  new LinkedList<Node>();
+			queue.add(root);
+			
+			while(!queue.isEmpty())
+			{
+				Node tempnode = queue.poll();
+				System.out.print(tempnode.data + " ");
+				if(tempnode.left !=null)
+					queue.add(tempnode.left);
+				if(tempnode.right!=null)
+					queue.add(tempnode.right);
+			}
+				
+		}
+		
+		void printInorder(Node node)
+		{
+			if(node == null)
+				return;
+			printInorder(node.left);
+			System.out.print(node.data + " ");
+			printInorder(node.right);
+		}
+	}
+	
+	public static void main(String[] args) {
+		LevelOrderTraversalQueue bfs = new LevelOrderTraversalQueue();
+		BinaryTree bt = bfs.new BinaryTree();
+		bt.root = bfs.new Node(1);
+		bt.root.right = bfs.new Node(3);
+		bt.root.left = bfs.new Node(2);
+		bt.root.left.right = bfs.new Node(5);
+		bt.root.left.left = bfs.new Node(4);
+		System.out.print("Inorder Traversal: ");
+		bt.printInorder(bt.root);
+		System.out.println();
+		System.out.print("BFS Traversal: ");
+		bt.printBFS(bt.root);
+	}
+
+}
